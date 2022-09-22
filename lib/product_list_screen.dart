3e773eb544821c164ebf4c71da313910860aa986
shihabcse1 <tomdshihab@gmail.com>
2022-09-22 14:30:34 +0600

    Card item design added with Add to cart button

--- lib/product_list_screen.dart
+++ lib/product_list_screen.dart
@@ -9,6 +9,20 @@ class ProductListScreen extends StatefulWidget {
 }
 
 class _ProductListScreenState extends State<ProductListScreen> {
+
+  List<String> productName = ['Mango' , 'Orange' , 'Grapes' , 'Banana' , 'Chery' , 'Peach','Mixed Fruit Basket',] ;
+  List<String> productUnit = ['KG' , 'Dozen' , 'KG' , 'Dozen' , 'KG' , 'KG','KG',] ;
+  List<int> productPrice = [10, 20 , 30 , 40 , 50, 60 , 70 ] ;
+  List<String> productImage = [
+    'https://image.shutterstock.com/image-photo/mango-isolated-on-white-background-600w-610892249.jpg' ,
+    'https://image.shutterstock.com/image-photo/orange-fruit-slices-leaves-isolated-600w-1386912362.jpg' ,
+    'https://image.shutterstock.com/image-photo/green-grape-leaves-isolated-on-600w-533487490.jpg' ,
+    'https://media.istockphoto.com/photos/banana-picture-id1184345169?s=612x612' ,
+    'https://media.istockphoto.com/photos/cherry-trio-with-stem-and-leaf-picture-id157428769?s=612x612' ,
+    'https://media.istockphoto.com/photos/single-whole-peach-fruit-with-leaf-and-slice-isolated-on-white-picture-id1151868959?s=612x612' ,
+    'https://media.istockphoto.com/photos/fruit-background-picture-id529664572?s=612x612' ,
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,6 +38,81 @@ class _ProductListScreenState extends State<ProductListScreen> {
             ),
           ),
           const SizedBox(width: 20.0,)
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+              child: ListView.builder(
+                itemCount: productName.length,
+                  itemBuilder: (context, index){
+                    return Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                Image(
+                                  height: 100,
+                                    width: 100,
+                                    image: NetworkImage(productImage[index].toString()),
+                                ),
+                                const SizedBox(width: 20,),
+                                Expanded(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                          productName[index].toString(),
+                                        style: const TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w500
+                                        ),
+                                      ),
+                                      const SizedBox(height: 5,),
+                                      Text(
+                                        productUnit[index].toString() + " "+r"$" + productPrice[index].toString(),
+                                        style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                                      ),
+                                      const SizedBox(height: 5,),
+                                      Align(
+                                        alignment: Alignment.centerRight,
+                                        child: Container(
+                                          height: 35,
+                                          width: 100,
+                                          decoration: const BoxDecoration(
+                                            color: Colors.green,
+                                          ),
+                                          child: const Center(
+                                            child: Text(
+                                              "Add to cart",
+                                              style: TextStyle(
+                                                color: Colors.white,
+                                              ),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  }
+              ),
+          ),
         ],
       ),
     );
